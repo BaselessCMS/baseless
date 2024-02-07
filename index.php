fd5321cffbf26c4eb85019f2ba089979e8d6592c
ControlledChaos <greg@ccdzine.com>
2024-02-07 15:31:53 -0800

    Clean up files.

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
 <?php
+/**
+ * CMS core file
+ *
+ * @package  JSON CMS
+ * @category Core
+ * @since    1.0.0
+ * @link     https://github.com/Bludiot/bludiot
+ */
 
-/*
- * Bludit
- * https://www.bludit.com
- * Author Diego Najar
- * Bludit is opensource software licensed under the MIT license.
-*/
+// Check if the CHS is installed.
+if ( ! file_exists( 'bl-content/databases/site.php' ) ) {
+	$base = dirname( $_SERVER['SCRIPT_NAME'] );
+	$base = rtrim( $base, '/' );
 
-// Check if Bludit is installed
-if (!file_exists('bl-content/databases/site.php')) {
-	$base = dirname($_SERVER['SCRIPT_NAME']);
-	$base = rtrim($base, '/');
-	$base = rtrim($base, '\\'); // Workaround for Windows Servers
-	header('Location:'.$base.'/install.php');
-	exit('<a href="./install.php">Install Bludit first.</a>');
+	// Work around for Windows servers.
+	$base = rtrim( $base, '\\' );
+
+	header( 'Location:' . $base . '/install.php' );
+	exit( '<a href="./install.php">Install the CMS first.</a>' );
 }
 
-// Load time init
-$loadTime = microtime(true);
+// Load time.
+$loadTime = microtime( true );
 
-// Security constant
-define('BLUDIT', true);
+// Security constant.
+define( 'BLUDIT', true );
+define( 'BLUDIT_PRO', true );
 
-// Directory separator
-define('DS', DIRECTORY_SEPARATOR);
+// Directory separator.
+define( 'DS', DIRECTORY_SEPARATOR );
 
-// PHP paths for init
-define('PATH_ROOT', __DIR__.DS);
-define('PATH_BOOT', PATH_ROOT.'bl-kernel'.DS.'boot'.DS);
+// PHP paths for init.
+define( 'PATH_ROOT', __DIR__ . DS );
+define( 'PATH_BOOT', PATH_ROOT . 'bl-kernel' . DS . 'boot' . DS );
 
 // Init
-require(PATH_BOOT.'init.php');
+require( PATH_BOOT . 'init.php' );
 
-// Admin area
-if ($url->whereAmI()==='admin') {
-	require(PATH_BOOT.'admin.php');
-}
-// Site
-else {
-	require(PATH_BOOT.'site.php');
+// Back end or front end.
+if ( $url->whereAmI() === 'admin' ) {
+	require( PATH_BOOT . 'admin.php' );
+} else {
+	require( PATH_BOOT . 'site.php' );
 }
