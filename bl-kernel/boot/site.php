aed963e7052a507570903ecc11d4b7f6e2a3c457
ControlledChaos <greg@ccdzine.com>
2024-02-22 09:39:12 -0800

    Code cleanup.

--- bl-kernel/boot/site.php
+++ bl-kernel/boot/site.php
@@ -1,35 +1,47 @@
-<?php defined('BLUDIT') or die('Bludit CMS.');
+<?php
+/**
+ * Set up front end
+ *
+ * @package  JSON CMS
+ * @category Core
+ * @since    1.0.0
+ */
 
-// Load plugins rules
-include(PATH_RULES.'60.plugins.php');
-
-// Plugins before all
-Theme::plugins('beforeAll');
-
-// Load rules
-include(PATH_RULES.'60.router.php');
-include(PATH_RULES.'69.pages.php');
-include(PATH_RULES.'99.header.php');
-include(PATH_RULES.'99.paginator.php');
-include(PATH_RULES.'99.themes.php');
-
-// Plugins before site loaded
-Theme::plugins('beforeSiteLoad');
-
-// Theme init.php
-if (Sanitize::pathFile(PATH_THEMES, $site->theme().DS.'init.php')) {
-	include(PATH_THEMES.$site->theme().DS.'init.php');
+// Stop if accessed directly.
+if ( ! defined( 'BLUDIT' ) ) {
+	die( 'You are not allowed to access this file directly.' );
 }
 
-// Theme HTML
-if (Sanitize::pathFile(PATH_THEMES, $site->theme().DS.'index.php')) {
-	include(PATH_THEMES.$site->theme().DS.'index.php');
+// Load plugins rules.
+include( PATH_RULES . '60.plugins.php' );
+
+// Plugins before all.
+Theme :: plugins( 'beforeAll' );
+
+// Load rules.
+include( PATH_RULES.'60.router.php' );
+include( PATH_RULES.'69.pages.php' );
+include( PATH_RULES.'99.header.php' );
+include( PATH_RULES.'99.paginator.php' );
+include( PATH_RULES.'99.themes.php' );
+
+// Plugins before site loaded.
+Theme :: plugins( 'beforeSiteLoad' );
+
+// Theme init.php.
+if ( Sanitize :: pathFile( PATH_THEMES, $site->theme() . DS . 'init.php' ) ) {
+	include( PATH_THEMES . $site->theme() . DS . 'init.php' );
+}
+
+// Theme HTML.
+if ( Sanitize :: pathFile( PATH_THEMES, $site->theme() . DS.'index.php' ) ) {
+	include( PATH_THEMES . $site->theme() . DS . 'index.php' );
 } else {
-	$L->p('Please check your theme configuration in the admin panel. Check for an active theme.');
+	$L->p( 'Please check your theme configuration in the admin panel. Check for an active theme.' );
 }
 
-// Plugins after site loaded
-Theme::plugins('afterSiteLoad');
+// Plugins after site loaded.
+Theme :: plugins( 'afterSiteLoad' );
 
-// Plugins after all
-Theme::plugins('afterAll');
+// Plugins after all.
+Theme :: plugins( 'afterAll' );
