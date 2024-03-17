ba1a950c6e81d6a9c2853c2dc6cd7313bc8e7048
ControlledChaos <greg@ccdzine.com>
2024-03-16 22:44:22 -0700

    Admin menu.

--- bl-kernel/admin/themes/default/html/sidebar.php
+++ bl-kernel/admin/themes/default/html/sidebar.php
@@ -5,17 +5,20 @@
 		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'dashboard' ?>"><span class="fa fa-dashboard"></span><?php $L->p('Dashboard') ?></a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" target="_blank" href="<?php echo HTML_PATH_ROOT ?>"><span class="fa fa-home"></span><?php $L->p('Website') ?></a>
-	</li>
-
-	<li class="nav-item mt-3">
-		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'new-content' ?>"><span style="color: #0078D4;" class="fa fa-plus-circle"></span><?php $L->p('New content') ?></a>
+		<a class="nav-link" href="<?php echo HTML_PATH_ROOT ?>"><span class="fa fa-home"></span><?php $L->p('Website') ?></a>
 	</li>
 
 	<?php if (!checkRole(array('admin'),false)): ?>
 	<li class="nav-item">
 		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'content' ?>"><span class="fa fa-archive"></span><?php $L->p('Content') ?></a>
 	</li>
+	<?php endif; ?>
+
+	<li class="nav-item">
+		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'new-content' ?>"><span class="fa fa-plus-circle"></span><?php $L->p('New content') ?></a>
+	</li>
+
+	<?php if (!checkRole(array('admin'),false)): ?>
 	<li class="nav-item">
 		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'edit-user/'.$login->username() ?>"><span class="fa fa-user"></span><?php $L->p('Profile') ?></a>
 	</li>
@@ -24,9 +27,6 @@
 	<?php if (checkRole(array('admin'),false)): ?>
 
 	<li class="nav-item mt-3">
-		<h4><?php $L->p('Manage') ?></h4>
-	</li>
-	<li class="nav-item">
 		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'content' ?>"><span class="fa fa-folder"></span><?php $L->p('Content') ?></a>
 	</li>
 
@@ -38,9 +38,6 @@
 	</li>
 
 	<li class="nav-item mt-3">
-		<h4><?php $L->p('Settings') ?></h4>
-	</li>
-	<li class="nav-item">
 		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'settings' ?>"><span class="fa fa-gear"></span><?php $L->p('General') ?></a>
 	</li>
 	<li class="nav-item">
@@ -49,7 +46,7 @@
 	<li class="nav-item">
 		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'themes' ?>"><span class="fa fa-desktop"></span><?php $L->p('Themes') ?></a>
 	</li>
-	<li class="nav-item">
+	<li class="nav-item mt-3">
 		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'about' ?>"><span class="fa fa-info"></span><?php $L->p('About') ?></a>
 	</li>
 
@@ -70,7 +67,7 @@
 
 	<?php endif; ?>
 
-	<li class="nav-item mt-5">
+	<li class="nav-item">
 		<a class="nav-link" href="<?php echo HTML_PATH_ADMIN_ROOT.'logout' ?>"><span class="fa fa-arrow-circle-right"></span><?php $L->p('Logout') ?></a>
 	</li>
 </ul>
