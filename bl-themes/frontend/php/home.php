3ec0d943b880d74ffcd7771049dae5446f2496bc
ControlledChaos <greg@ccdzine.com>
2024-03-16 22:27:51 -0700

    Replace default theme with old Dev Theme.

--- bl-themes/frontend/php/home.php
+++ bl-themes/frontend/php/home.php
@@ -1,74 +1,103 @@
-<?php if (empty($content)) : ?>
-  <div class="mt-4">
-    <?php $language->p('No pages found') ?>
-  </div>
-<?php endif ?>
+<main id="content" class="container" role="main">
+	<div class="row">
+		<div class="col-lg-10 col-xl-8 mr-auto ml-auto">
+			<section class="intro">
+				<div class="intro-meta">
+					<div class="inner">
+						<?php if ($site->slogan()): ?>
+						<h1><?php echo $helper->slogan(); ?></h1>
+						<?php endif ?>
+						<?php if ($site->description()): ?>
+						<p>
+							<?php echo $helper->description(); ?>
+						</p>
+						<?php endif ?>
+					</div>					
+				</div>
+			</section>
 
-<?php foreach ($content as $page) : ?>
-  <!-- Post -->
-  <div class="card my-5 border-0">
+			<h4 class="title"><?php echo $L->get('Latest posts') ?></h4>
+			<div class="loop">
+				<!-- Print all the content -->
+				<?php foreach ($content as $page): ?>
+				<article class="post item <?php echo $page->sticky() ? 'featured':''; ?>">
+					<div class="post-inner-content">
 
-    <!-- Load Bludit Plugins: Page Begin -->
-    <?php Theme::plugins('pageBegin'); ?>
+						<a href="<?php echo $page->permalink(); ?>" class="post-title" title="<?php echo $page->title(); ?>">
+							<h2>
+								<?php echo $page->title(); ?>
+							</h2>
+						</a>
+						<p>
+							<?php if(strlen($page->description())>0 ){
+									  echo  $page->description();
+								  }
+								  else{
+									  echo  $helper->content2excerpt($page->content(false), 250);
+								  }
+                            ?>
+						</p>
+					</div>
+					<div class="post-meta">
+						<time datetime="<?php echo $page->dateRaw('c') ?>">
+							<?php echo $page->date() ?>
+						</time>
+						<?php if ($page->category()):?>
+						<span class="tags">
+							<span class="entry-terms category">
+								<a href="<?php echo DOMAIN_CATEGORIES.$page->categoryKey() ?>" rel="tag">
+									<?php echo $page->category() ?>
+								</a>
+							</span>
+						</span>
+						<?php endif?>
+						<div class="inner">
+							<a href="https://twitter.com/share?text=<?php echo urlencode($page->title()) ?>&amp;url=<?php echo urlencode ($page->permalink()) ?>" class="twitter" onclick="window.open(this.href, 'share-twitter', 'width=550,height=235');return false;"
+								data-toggle="tooltip" data-placement="top" title="" data-original-title="Share on Twitter">
+								<i class="icon-twitter"></i>
+							</a>
+							<a href="#" class="read-later" data-toggle="tooltip" data-placement="top" data-id="<?php echo $page->slug() ?>" data-title="<?php echo $page->title() ?>" data-original-title="Read Later">
+								<i></i>
+							</a>
+						</div>
+					</div>
+				</article>
+				<?php endforeach ?>
+			</div>
 
-    <!-- Cover image -->
-    <?php if ($page->coverImage()) : ?>
-      <img class="card-img-top mb-3 rounded-0" alt="Cover Image" src="<?php echo $page->coverImage(); ?>" />
-    <?php endif ?>
+			<!-- Pagination -->
+			<?php if (Paginator::numberOfPages()>1): ?>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<nav class="pagination" role="navigation">
+							<div class="row">
+								<div class="col-4 text-left">
+									<?php if (Paginator::showPrev()):?>
+									<a class="newer-posts btn" href="<?php echo Paginator::previousPageUrl() ?>">
+										<?php echo $L->get('Newer'); ?>
+									</a>
+									<?php endif; ?>
+								</div>
+								<div class="col-4">
+									<span class="page-number">
+										<?php echo Paginator::currentPage() ?> / <?php echo Paginator::numberOfPages() ?>
+									</span>
+								</div>
+								<div class="col-4 text-right">
+									<?php if (Paginator::showNext()):?>
+									<a class="older-posts btn" href="<?php echo Paginator::nextPageUrl() ?>">
+										<?php echo $L->get('Older'); ?>
+									</a>
+									<?php endif; ?>
+								</div>
 
-    <div class="card-body p-0">
-      <!-- Title -->
-      <a class="text-dark" href="<?php echo $page->permalink(); ?>">
-        <h2 class="title"><?php echo $page->title(); ?></h2>
-      </a>
-
-      <!-- Creation date -->
-      <h6 class="card-subtitle mt-1 mb-4 text-muted">
-        <i class="bi bi-calendar"></i><?php echo $page->date(); ?>
-        <i class="ml-3 bi bi-clock-history"></i><?php echo $L->get('Reading time') . ': ' . $page->readingTime(); ?>
-      </h6>
-
-      <!-- Breaked content -->
-      <?php echo $page->contentBreak(); ?>
-
-      <!-- "Read more" button -->
-      <?php if ($page->readMore()) : ?>
-        <a href="<?php echo $page->permalink(); ?>"><?php echo $L->get('Read more'); ?></a>
-      <?php endif ?>
-
-    </div>
-
-    <!-- Load Bludit Plugins: Page End -->
-    <?php Theme::plugins('pageEnd'); ?>
-
-  </div>
-  <hr>
-<?php endforeach ?>
-
-<!-- Pagination -->
-<?php if (Paginator::numberOfPages() > 1) : ?>
-  <nav class="paginator">
-    <ul class="pagination flex-wrap">
-
-      <!-- Previous button -->
-      <?php if (Paginator::showPrev()) : ?>
-        <li class="page-item mr-2">
-          <a class="page-link" href="<?php echo Paginator::previousPageUrl() ?>" tabindex="-1">&#9664; <?php echo $L->get('Previous'); ?></a>
-        </li>
-      <?php endif; ?>
-
-      <!-- Home button -->
-      <li class="page-item <?php if (Paginator::currentPage() == 1) echo 'disabled' ?>">
-        <a class="page-link" href="<?php echo Theme::siteUrl() ?>">Home</a>
-      </li>
-
-      <!-- Next button -->
-      <?php if (Paginator::showNext()) : ?>
-        <li class="page-item ml-2">
-          <a class="page-link" href="<?php echo Paginator::nextPageUrl() ?>"><?php echo $L->get('Next'); ?> &#9658;</a>
-        </li>
-      <?php endif; ?>
-
-    </ul>
-  </nav>
-<?php endif ?>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+			<?php endif ?>
+		</div>
+	</div>
+</main>
