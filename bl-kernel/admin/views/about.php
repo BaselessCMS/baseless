eaeeb911ad84db014afcb3146190c18c41fdf9cd
ControlledChaos <greg@ccdzine.com>
2024-03-17 13:00:01 -0700

    Simplify dashboard, notifications to about page for now.

--- bl-kernel/admin/views/about.php
+++ bl-kernel/admin/views/about.php
@@ -1,6 +1,7 @@
-<?php
 
-echo Bootstrap :: pageTitle( [ 'title' => $L->g( 'Content Management System' ) ] );
+<h1><?php $L->p( 'System' ); ?></h1>
+
+<?php
 
 echo '<table class="table table-striped mt-3"><tbody>';
 
@@ -25,3 +26,28 @@ echo '<td></td>';
 echo '</tr>';
 
 echo '</tbody></table>';
+
+?>
+<?php if ( $config['dash_notify_qty'] > 0 ) : ?>
+
+<h2 class="m-0"><?php $L->p('Notifications') ?></h2>
+
+<ul class="list-group list-group-striped b-0">
+	<?php
+	$logs = array_slice($syslog->db, 0, NOTIFICATIONS_AMOUNT);
+	foreach ($logs as $log) {
+		$phrase = $L->g($log['dictionaryKey']);
+		echo '<li class="list-group-item">';
+		echo $phrase;
+		if (!empty($log['notes'])) {
+			echo ' « <b>' . $log['notes'] . '</b> »';
+		}
+		echo '<br><span class="notification-date"><small>';
+		echo Date::format($log['date'], DB_DATE_FORMAT, NOTIFICATIONS_DATE_FORMAT);
+		echo ' [ ' . $log['username'] . ' ]';
+		echo '</small></span>';
+		echo '</li>';
+	}
+	?>
+</ul>
+<?php endif; ?>
