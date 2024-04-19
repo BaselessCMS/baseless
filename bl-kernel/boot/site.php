04a20887b886d11f46b9c779099d86f5d3463d78
ControlledChaos <greg@ccdzine.com>
2024-04-18 19:56:26 -0700

    Minor spacing.

--- bl-kernel/boot/site.php
+++ bl-kernel/boot/site.php
@@ -20,11 +20,11 @@ include( PATH_RULES . '60.plugins.php' );
 Theme :: plugins( 'beforeAll' );
 
 // Load rules.
-include( PATH_RULES.'60.router.php' );
-include( PATH_RULES.'69.pages.php' );
-include( PATH_RULES.'99.header.php' );
-include( PATH_RULES.'99.paginator.php' );
-include( PATH_RULES.'99.themes.php' );
+include( PATH_RULES . '60.router.php' );
+include( PATH_RULES . '69.pages.php' );
+include( PATH_RULES . '99.header.php' );
+include( PATH_RULES . '99.paginator.php' );
+include( PATH_RULES . '99.themes.php' );
 
 // Plugins before site loaded.
 Theme :: plugins( 'beforeSiteLoad' );
@@ -35,7 +35,7 @@ if ( Sanitize :: pathFile( PATH_THEMES, $site->theme() . DS . 'init.php' ) ) {
 }
 
 // Theme HTML.
-if ( Sanitize :: pathFile( PATH_THEMES, $site->theme() . DS.'index.php' ) ) {
+if ( Sanitize :: pathFile( PATH_THEMES, $site->theme() . DS . 'index.php' ) ) {
 	include( PATH_THEMES . $site->theme() . DS . 'index.php' );
 } else {
 	$L->p( 'Please check your theme configuration in the admin panel. Check for an active theme.' );
