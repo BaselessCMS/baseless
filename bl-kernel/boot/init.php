23d4a287ca53ca6496ddfdd4abfef89f8b74c9c5
Controlled Chaos Design <greg@ccdzine.com>
2025-11-15 13:47:35 -0800

    Remove unnecessary file.

--- bl-kernel/boot/init.php
+++ bl-kernel/boot/init.php
@@ -147,8 +147,112 @@ define( 'DB_SYSLOG', PATH_DATABASES . 'syslog.php' );
 define( 'DB_USERS', PATH_DATABASES . 'users.php' );
 define( 'DB_SECURITY', PATH_DATABASES . 'security.php' );
 
-// User environment variables.
-include( PATH_KERNEL . 'boot' . DS . 'config.php' );
+// Log.
+define( 'LOG_SEP', $config['log']['sep'] );
+define( 'LOG_TYPE_INFO', $config['log']['info'] );
+define( 'LOG_TYPE_WARN', $config['log']['warn'] );
+define( 'LOG_TYPE_ERROR', $config['log']['error'] );
+
+// Protecting against Symlink attacks.
+define( 'CHECK_SYMBOLIC_LINKS', $config['symlink'] );
+
+// Alert status OK.
+define( 'ALERT_STATUS_OK', $config['alert']['ok'] );
+
+// Alert status fail.
+define( 'ALERT_STATUS_FAIL', $config['alert']['fail'] );
+
+// Alert notification disappear in X seconds.
+define( 'ALERT_DISAPPEAR_IN', $config['alert']['seconds'] );
+
+// Items per page for admin area.
+define( 'ITEMS_PER_PAGE_ADMIN', $config['admin']['per_page'] );
+
+// Password length.
+define( 'PASSWORD_LENGTH', $config['login']['pw_length'] );
+
+// Password salt length.
+define( 'SALT_LENGTH', $config['login']['salt_length'] );
+
+// Remember me.
+define( 'REMEMBER_COOKIE_USERNAME', $config['login']['cookie_user'] );
+define( 'REMEMBER_COOKIE_TOKEN', $config['login']['cookie_token'] );
+define( 'REMEMBER_COOKIE_EXPIRE_IN_DAYS', $config['login']['cookie_expire'] );
+
+// Token time to live for login via email. The offset is defined by http://php.net/manual/en/datetime.modify.php.
+define( 'TOKEN_EMAIL_TTL', $config['login']['email_token'] );
+
+// Session lifetime of the cookie in seconds which is sent to the browser
+// The value 0 means until the browser is closed.
+define( 'SESSION_COOKIE_LIFE_TIME', $config['login']['session_life'] );
+
+// Session timeout server side, gc_maxlifetime.
+// 3600 = 1hour
+define( 'SESSION_GC_MAXLIFETIME', $config['login']['session_max'] );
+
+// Database date format.
+define( 'DB_DATE_FORMAT', $config['dates']['db'] );
+
+// Database date format.
+define( 'BACKUP_DATE_FORMAT', $config['dates']['backup'] );
+
+// Sitemap date format.
+define( 'SITEMAP_DATE_FORMAT', $config['dates']['db'] );
+
+// Date format for Manage Content, Manage Users.
+define( 'ADMIN_PANEL_DATE_FORMAT', $config['dates']['admin'] );
+
+// Date format for Dashboard schedule posts.
+define( 'SCHEDULED_DATE_FORMAT', $config['dates']['schedule'] );
+
+// Notifications date format.
+define( 'NOTIFICATIONS_DATE_FORMAT', $config['dates']['notify'] );
+
+// Manage content date format.
+define( 'MANAGE_CONTENT_DATE_FORMAT', $config['dates']['manage'] );
+
+// Amount of items to show on notification panel.
+define( 'NOTIFICATIONS_AMOUNT', $config['dash_notify_qty'] );
+
+// Charset, default UTF-8.
+define( 'CHARSET', $config['charset'] );
+
+// Permissions for new directories.
+define( 'DIR_PERMISSIONS', (int)$config['dir_permission'] );
+
+// Admin URI filter to access to the admin panel.
+define( 'ADMIN_URI_FILTER', $config['admin']['uri'] );
+
+// Default language file, in this case is English.
+define( 'DEFAULT_LANGUAGE_FILE', $config['default_lang'] );
+
+// Allowed image extensions.
+$GLOBALS['ALLOWED_IMG_EXTENSION'] = $config['media']['img_ext'];
+
+// Allowed image mime types.
+$GLOBALS['ALLOWED_IMG_MIMETYPES'] = $config['media']['img_mime'];
+
+// Number of images to show in the media manager per page.
+define( 'MEDIA_MANAGER_NUMBER_OF_FILES', $config['media']['per_page'] );
+
+// Sort the image by date.
+define( 'MEDIA_MANAGER_SORT_BY_DATE', $config['media']['sort_date'] );
+
+// Profile image size.
+define( 'PROFILE_IMG_WIDTH', $config['media']['avatar_width'] );
+define( 'PROFILE_IMG_HEIGHT', $config['media']['avatar_height'] );
+define( 'PROFILE_IMG_QUALITY', $config['media']['avatar_quality'] ); // 100%
+
+// Content filename.
+define( 'FILENAME', $config['content']['db_file'] );
+
+// Page brake string.
+define( 'PAGE_BREAK', $config['content']['page_break'] );
+
+// Constant arrays using define are not allowed in PHP 5.6 or earlier.
+
+// Type of pages included in the tag database.
+$GLOBALS['DB_TAGS_TYPES'] = $config['content']['tags_types'];
 
 // Set internal character encoding.
 mb_internal_encoding( CHARSET );
